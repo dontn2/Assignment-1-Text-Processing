39a9d1c36ec74bfd164e6bf13a73907dc71d34fb
Don Nguyen <dontn2@uci.edu>
2024-04-05 19:11:33 -0700

    tokenize implemented

--- PartA.py
+++ PartA.py
@@ -2,6 +2,20 @@ def tokenize(textFilePath) -> list:
     """
     Runtime Complexity:
     """
+    tokenList = []
+    token = ""
+    with open(textFilePath) as text:
+        while True:
+            char = text.read(1)
+            if char.isalnum():
+                token = token + char.lower()
+            else:
+                tokenList.append(token)
+                token = ""
+                if char == "":
+                    break
+    return tokenList
+
 
 def computeWordFrequencies(tokenList) -> dict:
     """
@@ -12,3 +26,8 @@ def printFrequencies(tokenCount) -> None:
     """
     Runtime Complexity:
     """
+
+if __name__ == "__main__":
+    tokenList = tokenize("text1.txt")
+    for token in tokenList:
+        print(token)
