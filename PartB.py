cf772fc0dd1c79d913d56e9bb1170e9e89aedce8
Don Nguyen <dontn2@uci.edu>
2024-04-07 14:24:56 -0700

    CountCommonTokens and removeDuplicates added

--- PartB.py
+++ PartB.py
@@ -1,2 +1,22 @@
-def countCommonTokens(file1Tokens, file2Tokens):
-    pass
+import PartA
+
+def countCommonTokens(file1Tokens, file2Tokens) -> int:
+    count = 0
+    file1Tokens = removeDuplicates(file1Tokens)
+    for token in file1Tokens:
+        if token in file2Tokens:
+            count += 1
+    return count
+
+def removeDuplicates(givenList):
+    result = []
+    for element in givenList:
+        if element not in result:
+            result.append(element)
+    return result 
+    
+
+if __name__ == "__main__":
+    tokenList1 = PartA.tokenize("canvas1.txt")
+    tokenList2 = PartA.tokenize("canvas2.txt")
+    print(countCommonTokens(tokenList1, tokenList2))
